e9d939a8ce34f28b28c8cfbdfe8ed48d6ffdc121
autolii-yong <yong.zheng@autolii.com>
2019-12-20 12:38:37 -0500

    change the hero and nav theme

--- resources/views/common/nav.blade.php
+++ resources/views/common/nav.blade.php
@@ -1,17 +1,60 @@
-<nav class="level">
-  <p class="level-item has-text-centered">
-    <a class="link is-info">Home</a>
-  </p>
-  <p class="level-item has-text-centered">
-    <a class="link is-info">Menu</a>
-  </p>
-  <p class="level-item has-text-centered">
-    <img src="{{url('images/dda_logo_transparent.png')}}" alt="DDA" >
-  </p>
-  <p class="level-item has-text-centered">
-    <a class="link is-info">Reservations</a>
-  </p>
-  <p class="level-item has-text-centered">
-    <a class="link is-info">Contact</a>
-  </p>
+<nav class="navbar is-spaced is-transparent" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="{{url('/')}}">
+      <img src="{{url('images/dda_logo_transparent.png')}}" alt="DDA">
+    </a>
+
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        Home
+      </a>
+
+      <a class="navbar-item">
+        Documentation
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          More
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            About
+          </a>
+          <a class="navbar-item">
+            Jobs
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <span class="icon is-small">
+                <i class="fas fa-dollar-sign"></i>
+            </span>
+            <strong>Get an estimate</strong>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </nav>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,12 +4,16 @@
 @section('content')
     <section class="hero is-large dda-homepage-hero">
         <div class="hero-body has-text-centered">
-            <div class="container">
-                <h1 class="title has-text-white">
-                    Primary bold title
-                </h1>
-              
+        <div class="container">
+                <h1 class="title has-text-weight-bold has-text-white is-uppercase">Directional Drilling Company</h1>
+                <button class="button is-info">
+                    <span class="icon is-small">
+                        <i class="fas fa-clock"></i>
+                    </span>
+                    <span>Check</span>
+                </a>
             </div>
+        </div>
     </section>
 @endsection
 
