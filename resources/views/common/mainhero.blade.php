4d2bc7b13d859d4256d8b33ff8146bf015d720d3
autolii-yong <yong.zheng@autolii.com>
2019-12-21 10:28:21 -0500

    abstract out main hero

---
+++ resources/views/common/mainhero.blade.php
@@ -0,0 +1,28 @@
+<section class="hero is-large dda-homepage-hero">
+        <div class="hero-body has-text-centered">
+            <div class="container">
+                <h1 class="title has-text-weight-bold has-text-white is-uppercase">Directional Drilling Company</h1>
+                <h3 class="subtitle has-text-weight-bold has-text-white is-capitalize">Check if we service your county</h3>
+                <div class="columns">
+                    <div class="column is-4 is-offset-4">
+                    <form>
+                        <div class="typeahead__container">
+                            <div class="typeahead__field">
+                                <div class="typeahead__query">
+                                    <input class="js-typeahead"
+                                        name="q"
+                                        autocomplete="off">
+                                </div>
+                                <div class="typeahead__button">
+                                    <button type="submit">
+                                        <span class="typeahead__search-icon"></span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    </div>           
+                </div>
+            </div>
+        </div>
+    </section>
