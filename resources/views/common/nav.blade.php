aa75007c0ce317665e2f6d32d08fff950c646f65
autolii-yong <yong.zheng@autolii.com>
2019-12-31 17:58:07 -0500

    committ nav

--- resources/views/common/nav.blade.php
+++ resources/views/common/nav.blade.php
@@ -1,57 +1,42 @@
-<nav class="navbar is-spaced is-transparent" role="navigation" aria-label="main navigation">
+<nav class="navbar is-spaced is-transparent is-fixed-top" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="{{url('/')}}">
       <img src="{{url('images/dda_logo_transparent.png')}}" alt="DDA">
     </a>
 
-    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="dda-nav">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
 
-  <div id="navbarBasicExample" class="navbar-menu">
+  <div id="dda-nav" class="navbar-menu">
     <div class="navbar-start">
-      <a class="navbar-item">
-        Home
+      <a class="navbar-item is-uppercase">
+        Who we are
       </a>
 
-      <a class="navbar-item">
-        Documentation
+      <a class="navbar-item is-uppercase">
+        What we do
       </a>
-
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
-
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
-        </div>
-      </div>
+      <a class="navbar-item is-uppercase">
+        Project Gallery
+      </a>
+      <a class="navbar-item is-uppercase">
+        Contact Us
+      </a>
     </div>
+     
 
     <div class="navbar-end">
       <div class="navbar-item">
         <div class="buttons">
-          <a class="button is-primary">
+          <a class="button is-primary is-outlined">
             <span class="icon is-small">
                 <i class="fas fa-dollar-sign"></i>
             </span>
-            <strong>Get an estimate</strong>
+            <strong class="is-uppercase">Get an estimate</strong>
           </a>
         </div>
       </div>
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
-<html  lang="{{ app()->getLocale() }}"><head>
+<html class="has-navbar-fixed-top" lang="{{ app()->getLocale() }}">
     <head>
-    <meta charset="utf-8">
+        <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
 
